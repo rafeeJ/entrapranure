b9d1837d0c17e94e230e8977aef5ebfb1ddb3d2b
Rafee Jenkins <rafeejenkins@gmail.com>
2022-07-11 22:55:28 +0100

    Added tab bar

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const Tab = createBottomTabNavigator()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Hello} />
+        <Tab.Screen name="Stonks" component={Hello} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
+}
+
+function Hello() {
+  return(
+    <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+  )
 }
 
 const styles = StyleSheet.create({
